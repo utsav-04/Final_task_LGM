9a74e9f6b8861d6b5822a17bfc7a6adfbc5f5566
Utsav Raj <100127451+utsav-04@users.noreply.github.com>
2022-08-28 15:31:56 +0530

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+    <title>About</title>
+<link rel="stylesheet" href="../csss/aboutus.css" type="text/css">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=DynaPuff&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css">
+
+</head>
+<body>
+    <header>
+      <nav>
+        <div class="row clearfix">
+            <ul class="main-nav" animate slideInDown>
+                <li><a href="../index.php">HOME</a></li>
+                <li><a href="aboutus.php">ABOUT</a></li>
+                <li><a href="contactus.php">CONTACT</a></li>
+                <li><a href="../login.php">ADMIN LOGIN</a></li>
+          </ul>
+        </div>
+      </nav>
+      <div class="main-content-header">
+          <h2 class="search">About Us</h2>
+             <div class="div2">
+             <h3>Student Result Management System</h2>
+             <p>Student result management system is a technological opportunity for the school, college, university and coaching center institutions searching for a secure, simple and alternative solution to the conventional paper-based exam results evaluation, reporting and distribution. It helps to manage the access to result related facilities to teachers and students.</p> 
+             
+             </div>  
+          </div>
+    </header>
+    
+</body>
+</html>
